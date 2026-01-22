f6e83bb53afa012978b14fbec51b6822dc11229d
prof-palm <mitchell.runs@gmail.com>
2026-01-21 22:06:27 -0700

    Implemented the reset board function

---
+++ shared/src/main/java/chess/PieceMoveCalculator.java
@@ -0,0 +1,38 @@
+package chess;
+
+import java.util.List;
+
+public class PieceMoveCalculator
+{
+     ;
+
+
+    class BishopMoves extends PieceMoveCalculator{
+
+        public List Bishops_moves(ChessPosition startPosition){
+            chess.ChessPosition position = getStartPosition();
+
+
+
+
+
+
+        }
+
+    }
+    class RookMoves extends PieceMoveCalculator{
+
+    }
+    class PawnMoves extends PieceMoveCalculator{
+
+    }
+    class KnightMoves extends PieceMoveCalculator{
+
+    }
+    class QueenMoves extends PieceMoveCalculator{
+
+    }
+    class KingMoves extends PieceMoveCalculator{
+
+    }
+}
