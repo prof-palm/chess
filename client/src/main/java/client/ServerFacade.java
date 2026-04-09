4de8297fd7e8a1e6a53f5ff9a3de1902d6e67511
prof-palm <mpalme11@byu.edu>
2026-04-08 22:01:51 -0600

    Implemented basic websocket in ServerFacade

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -16,13 +16,45 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+
+import com.google.gson.Gson;
+
+import jakarta.websocket.*;
+import websocket.messages.ServerMessage;
+
+import javax.management.NotificationFilter;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+
 public class ServerFacade {
     private final HttpClient client = HttpClient.newHttpClient();
     private final String serverUrl;
+    private Session session;
+    private NotificationHandler notificationHandler ;
 
 
-    public ServerFacade(String url) {
+    public ServerFacade(String url) throws ResponseException{
         serverUrl = url;
+        try {
+            url = url.replace("http", "ws");
+            URI socketURI = new URI(url + "/ws");
+
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            this.session = container.connectToServer(this, socketURI);
+
+            //set message handler
+            this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+                @Override
+                public void onMessage(String message) {
+                    ServerMessage notification = new Gson().fromJson(message, ServerMessage.class);
+                    notificationHandler.notify(notification);
+                }
+            });
+        } catch (DeploymentException | IOException | URISyntaxException ex) {
+            throw new ResponseException(ResponseException.Code.ServerError, ex.getMessage());
+        }
 
     }
 
